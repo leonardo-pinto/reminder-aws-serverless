a1a508da47aa35363394cf5ec800d560a4a2b99f
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-09-28 16:33:18 -0400

    feat(fetchReminderFunction): add dynamodb query to retrieve reminders by user

--- lambda/reminders/fetchReminderFunction.ts
+++ lambda/reminders/fetchReminderFunction.ts
@@ -3,15 +3,35 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
+
+const tableDdb = process.env.TABLE_DDB!;
+const dbClient = new DynamoDB.DocumentClient();
+
+interface ReminderResponse {
+  content: string;
+  email: string;
+  reminderDate: string;
+}
 
 export async function handler(
   event: APIGatewayProxyEvent,
-  context: Context
+  _context: Context
 ): Promise<APIGatewayProxyResult> {
-  console.log(`API Gateway RequestID: ${event.requestContext.requestId}`);
+  const data = await dbClient
+    .query({
+      TableName: tableDdb,
+      KeyConditionExpression: `pk = :userId AND begins_with(sk, :prefix)`,
+      ExpressionAttributeValues: {
+        ":userId": `userId#${event.requestContext.authorizer!.claims.sub}`,
+        ":prefix": "reminderId",
+      },
+      ProjectionExpression: "content, reminderDate, email",
+    })
+    .promise();
 
   return {
     statusCode: 200,
-    body: "OK",
+    body: JSON.stringify(data.Items as ReminderResponse[]),
   };
 }
