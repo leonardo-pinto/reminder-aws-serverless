ae9f8ee5df1b9edd04cae5464753d2f645badd8f
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-09-28 16:33:44 -0400

    feat(writeReminderFunction): add temp impl to create reminder without proper ttl

--- lambda/reminders/writeReminderFunction.ts
+++ lambda/reminders/writeReminderFunction.ts
@@ -3,15 +3,70 @@ import {
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
+import { v4 as uuid } from "uuid";
+
+const tableDdb = process.env.TABLE_DDB!;
+const dbClient = new DynamoDB.DocumentClient();
+
+interface Reminder {
+  content: string;
+  reminderDate: string;
+  email?: string;
+}
+
+interface ReminderRepository {
+  pk: string;
+  sk: string;
+  ttl: number;
+  content: string;
+  email: string;
+  reminderDate: string;
+  createdAt: number;
+}
 
 export async function handler(
   event: APIGatewayProxyEvent,
-  context: Context
+  _context: Context
 ): Promise<APIGatewayProxyResult> {
-  console.log(`API Gateway RequestID: ${event.requestContext.requestId}`);
+  console.log(`EVENT: ${JSON.stringify(event)}`);
+  try {
+    const reminderRequest = JSON.parse(event.body!) as Reminder;
 
-  return {
-    statusCode: 200,
-    body: "OK",
-  };
+    // TODO ADD TTL CALCULATION BASED ON INPUT
+    const timestamp = Date.now();
+    const ttl = ~~(timestamp / 1000 + 1 * 60);
+    const reminderId = uuid();
+
+    const reminder: ReminderRepository = {
+      pk: `userId#${event.requestContext.authorizer!.claims.sub}`,
+      sk: `reminderId#${reminderId}`,
+      ttl,
+      content: reminderRequest.content,
+      email: event.requestContext.authorizer!.claims.email,
+      reminderDate: reminderRequest.reminderDate,
+      createdAt: timestamp,
+    };
+
+    await dbClient
+      .put({
+        TableName: tableDdb,
+        Item: reminder,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        ...reminderRequest,
+        email: reminder.email,
+      }),
+    };
+  } catch (error) {
+    console.error((error as Error).message);
+    return {
+      statusCode: 400,
+      body: (error as Error).message,
+    };
+  }
 }
