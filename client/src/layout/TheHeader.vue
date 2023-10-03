b0fc7051742c086e4e8dd3c263cf23adb36edffb
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-10-03 18:43:50 -0400

    feat(client): add TheHeader component

---
+++ client/src/layout/TheHeader.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <img src="../assets/app-logo.png" alt="app logo" />
+    <div>
+      <p>Hello, {{ props.name }}</p>
+      <button id="sign-out-btn">
+        <i
+          class="pi pi-sign-out"
+          style="font-size: 1.5rem"
+          @click="$emit('handleSignout')"
+        ></i>
+      </button>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  name: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+header {
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 2rem;
+  padding: 0 1rem;
+}
+
+header div {
+  align-items: center;
+  display: flex;
+}
+
+img {
+  padding: 1rem;
+  width: 10rem;
+}
+
+#sign-out-btn {
+  background: none;
+  border: none;
+  cursor: pointer;
+  margin-left: 1rem;
+}
+</style>
