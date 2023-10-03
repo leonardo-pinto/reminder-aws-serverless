c65d1c21f89357a43d6278e65e081585cc89f601
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-10-03 18:43:00 -0400

    feat(client): add prime vue components to main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,17 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
 import { Amplify } from "aws-amplify";
 import AmplifyVue from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
 import PrimeVue from "primevue/config";
 import Button from "primevue/button";
+import Dialog from "primevue/dialog";
+import Calendar from "primevue/calendar";
+import InputText from "primevue/inputtext";
+import Card from "primevue/card";
+import ProgressSpinner from "primevue/progressspinner";
 import "primevue/resources/themes/lara-light-indigo/theme.css";
+import "primeicons/primeicons.css";
 
 Amplify.configure({
   Auth: {
@@ -22,4 +27,9 @@ app.use(PrimeVue);
 app.use(AmplifyVue);
 
 app.component("Button", Button);
+app.component("Dialog", Dialog);
+app.component("Calendar", Calendar);
+app.component("InputText", InputText);
+app.component("Card", Card);
+app.component("ProgressSpinner", ProgressSpinner);
 app.mount("#app");
