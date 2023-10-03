8cf92a6a68bb20bac584a47903dbb893874bb7c1
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-10-03 18:44:52 -0400

    feat(client): add components to App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,31 @@
 <template>
-  <nav>Welcome to ReminderApp</nav>
-  <Button>Hello World</Button>
   <authenticator
     :services="services"
     :login-mechanisms="['email']"
     :sign-up-attributes="['name']"
   >
-    <template v-slot="{ user, signOut }">
-      <h1>Hello {{ user.attributes.name }}</h1>
-      <button @click="signOut">Sign Out</button>
+    <template v-slot="{ signOut, user }">
+      <TheHeader
+        @handle-signout="signOut"
+        :name="user.attributes.name"
+      ></TheHeader>
+      <Button
+        id="open-dialog-btn"
+        label="Create new reminder"
+        size="small"
+        icon="pi pi-calendar-times"
+        @click="showCreateReminder = true"
+      ></Button>
+      <Dialog
+        v-model:visible="showCreateReminder"
+        modal
+        header="Create new reminder"
+        id="dialog"
+      >
+        <CreateReminder @close="showCreateReminder = false"></CreateReminder
+      ></Dialog>
+      <RemindersList></RemindersList>
+      <TheFooter></TheFooter>
     </template>
   </authenticator>
 </template>
@@ -16,18 +33,30 @@
 <script setup lang="ts">
 import { Authenticator } from "@aws-amplify/ui-vue";
 import { Auth } from "aws-amplify";
+import TheHeader from "./layout/TheHeader.vue";
+import TheFooter from "./layout/TheFooter.vue";
+import RemindersList from "./components/RemindersList.vue";
+import CreateReminder from "./components/CreateReminder.vue";
+import { ref } from "vue";
 
 const services = {
   async handleSignIn(formData: any) {
-    console.log("handle sign in");
-    console.log(formData);
     const input = {
       username: formData.username as string,
       password: formData.password as string,
     };
     const result = await Auth.signIn(input);
-    console.log(`RESULT SIGN IN: ${result.signInUserSession.idToken.jwtToken}`);
+    localStorage.setItem("token", result.signInUserSession.idToken.jwtToken);
     return result;
   },
 };
+
+const showCreateReminder = ref(false);
 </script>
+
+<style>
+#open-dialog-btn {
+  display: flex;
+  margin: 0 auto;
+}
+</style>
